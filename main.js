d48c249888d0b154de704eda43da63ac9358d5aa
Matheus Holstein <themathpires@gmail.com>
2019-06-23 15:36:28 +0100

    moving divs

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ btncolors.map(function(button) {
     button.addEventListener('click', function(){
         console.log(button);
         const color = button.lastElementChild.textContent.toLowerCase();
-        
-        //product.src='./assets/images/' + color + '-model.png';
         product.src=`./assets/images/${color}-model.png`;
-
     });
 });
+
+let rightSide = document.getElementsByClassName('info');
+let leftSide = document.getElementsByClassName('product'); 
+let mainRow = Array.from(document.getElementsByClassName('row'))[0];
+
+document.querySelector('.container .row').parentNode.removeChild(rightSide);
+
+// Create a new element
+//var newColumn = document.createElement('div');
+
+// Get the reference node
+//var referenceNode = document.querySelector('.info');
+
+// Insert the new node before the reference node
+//referenceNode.before(newColumn);
